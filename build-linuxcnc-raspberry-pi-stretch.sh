3a0f4eff78eb8abb2f2bdd63d7aa60d20a158f76
jefffall <jeffreyefall@gmail.com>
2018-03-09 21:32:50 -0800

    Add files via upload

--- build-linuxcnc-raspberry-pi-stretch.sh
+++ build-linuxcnc-raspberry-pi-stretch.sh
@@ -1,30 +1,30 @@
 #apt-cache dump | grep -oP 'Package: \K.*' | sort -n | grep cnc
-apt-get install git
+apt-get -y install git
 git clone git://github.com/linuxcnc/linuxcnc.git linuxcnc-dev
-apt-get install autoconf
-apt-get install libudev-dev
-apt-get install libmodbus-dev
-apt-get install libusb-1.0-0-dev
-apt-get install libglib2.0-dev
-apt-get install tcl-dev tk-dev
-apt-get install bwidget
-apt-get install libtk-img
-apt-get install tclx
-apt-get install python-gtk2
-apt-get install libreadline-dev
-apt-get install python-tk
-apt-get install libboost-all-dev
-apt-get install screen
-screen apt-get install libboost-all-dev
-apt-get install python-opengl
-apt-get install python-opengl-devel
-apt-get install libmesa-dev
-apt-get install libglu1-mesa-dev
-apt-get install libxmu-dev
-apt-get install yapps
-apt-get install yapps2 yapps2-runtime
-apt-get install libgtk2.0-dev
-apt install intltool
+apt-get -y install autoconf
+apt-get -y install libudev-dev
+apt-get -y install libmodbus-dev
+apt-get -y install libusb-1.0-0-dev
+apt-get -y install libglib2.0-dev
+apt-get -y install tcl-dev tk-dev
+apt-get -y install bwidget
+apt-get -y install libtk-img
+apt-get -y install tclx
+apt-get -y install python-gtk2
+apt-get -y install libreadline-dev
+apt-get -y install python-tk
+apt-get -y install libboost-all-dev
+apt-get -y install screen
+screen apt-get -y install libboost-all-dev
+apt-get -y install python-opengl
+apt-get -y install python-opengl-devel
+apt-get -y install libmesa-dev
+apt-get -y install libglu1-mesa-dev
+apt-get -y install libxmu-dev
+apt-get -y install yapps
+apt-get -y install yapps2 yapps2-runtime
+apt-get -y install libgtk2.0-dev
+apt install -y intltool
 cd linuxcnc-dev/src
 ./autogen.sh
 #./configure --with-realtime=uspace --disable-gtk --enable-non-distributable=yes --disable-python
